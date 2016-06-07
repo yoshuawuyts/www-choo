effac7ddcb3cd12d911452cbecc8fe9a09a08306
Yoshua Wuyts <i@yoshuawuyts.com>
2016-06-07 17:53:15 +0200

    related: add

--- views/related.js
+++ views/related.js
@@ -1,22 +1,38 @@
 const view = require('bel')
 
-const content = `
-`
+const content = [
+  { name: 'pull-stream', url: 'https://github.com/pull-stream/pull-stream' },
+  { name: 'stack.gl', url: 'http://stack.gl' },
+  { name: 'yo-yo', url: 'https://github.com/maxogden/yo-yo' },
+  { name: 'bel', url: 'https://github.com/shama/bel' }
+]
 
 module.exports = relatedView
 
 const bgColor = '#fff'
+const title = 'Friends'
 
 function relatedView () {
   return view`
     <section
       class="flex justify-center mx-auto"
       style="background-color: ${bgColor}">
-      <div class="left-align px2" style="width: 32rem;">
-        <pre class="f6 code lh-copy">
-          ${content}
-        </pre>
+      <div class="flex left-align px2 max-width-2">
+        <h1 class="f3 bold code mr4 lh-copy">${title}</h1>
+        <div>
+          ${createList(content)}
+        </div>
       </div>
     </section>
   `
+}
+
+function createList (data) {
+  return data.map((pair) => {
+    return view`
+      <a href=${pair.url} class="col-6 inline-block f3 f4-ns code lh-copy">
+        ${pair.name}
+      </a>
+    `
+  })
 }
